ae2d82562431f01392fb89b7c71203254248521a
Vijay Maddi <vijaykumar.maddi@gmail.com>
2021-05-29 22:42:19 +0530

    Part 2: Solution

--- src/main/java/RestaurantService.java
+++ src/main/java/RestaurantService.java
@@ -11,7 +11,7 @@ public class RestaurantService {
                 return restaurant;
             }
         }
-        throw  new restaurantNotFoundException(restaurantName);
+        throw  new restaurantNotFoundException("Restaurant "+ restaurantName+" could not be found");
     }
 
 
